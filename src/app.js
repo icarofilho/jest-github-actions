897036b6959f92040940b331b1c8d5b52fd18c4a
Icaro Ferreira <icaro.ferreira.filho@gmail.com>
2022-04-12 16:36:14 -0300

    nova rota

--- src/app.js
+++ src/app.js
@@ -7,4 +7,8 @@ app.get('/test',(req,res)=>{
   res.status(200).send("Hello World")
 })
 
+app.get("/", (req, res) => {
+    res.status(200).send("Bem-vindo ao meu servidor")
+});
+
 module.exports = app
